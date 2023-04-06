6ed05e4de0c824b854b73e419c2dcf45971c3603
Vasylku <vasylkuvayskov@gmail.com>
2023-04-06 10:00:18 -0500

    routes send se data

---
+++ ticketRouter.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const tickets = require('./ticket.json');
+
+router.use(bodyParser.json());
+
+// Endpoint to get all tickets
+router.get('/rest/list', (req, res) => {
+    res.send(tickets);
+});
+
+// Endpoint to get a single ticket by id
+router.get('/rest/ticket/:id', (req, res) => {
+    const id = req.params.id;
+    const ticket = tickets.find((t) => t.id === id);
+
+    if (!ticket) {
+        res.status(404).send('Ticket not found');
+    } else {
+        res.send(ticket);
+    }
+});
+
+// Endpoint to create a new ticket
+router.post('/rest/ticket', (req, res) => {
+    const {
+        type,
+        subject,
+        description,
+        priority,
+        status,
+        creator,
+        created_at,
+        updated_at,
+    } = req.body;
+
+    if (!type || !subject || !description || !priority || !status || !creator || !created_at || !updated_at) {
+        res.status(400).send('Incomplete ticket info');
+    } else {
+        const newTicket = {
+            id: Date.now().toString(),
+            type,
+            subject,
+            description,
+            priority,
+            status,
+            creator,
+            created_at,
+            updated_at,
+        };
+        tickets.push(newTicket);
+        res.send(newTicket);
+    }
+});
+
+module.exports = router;
