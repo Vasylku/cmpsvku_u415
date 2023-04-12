1407fe995eacde08b4b73f4f24ec74334bf6701e
Vasylku <vasylkuvayskov@gmail.com>
2023-04-11 22:25:29 -0500

    updated post/form

--- scripts.js
+++ scripts.js
@@ -1,12 +1,58 @@
 
+function submitForm() {
+    const type = document.getElementById("type").value;
+    const subject = document.getElementById("subject").value;
+    const description = document.getElementById("description").value;
+    const priority = document.getElementById("priority").value;
+    const status = document.getElementById("status").value;
+    const creator = document.getElementById("creator").value;
+    const created_at = document.getElementById("created_at").value;
 
+    if (!type || !subject || !description || !priority || !status || !creator || !created_at) {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    const data = {
+        type,
+        subject,
+        description,
+        priority,
+        status,
+        creator,
+        created_at,
+    };
+
+    fetch('/rest/ticket', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            if (response.ok) {
+                alert('Ticket created successfully!');
+                return response.json();
+            } else {
+                throw new Error('Network response was not ok');
+            }
+        })
+        .then(json => {
+            console.log(json);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
 function fetchAllTickets() {
+
+    setTimeout(() => {
         fetch('/rest/list')
             .then(response => response.json())
             .then(data => {
              
                 const table = document.getElementById('ticket-table');
-                // Loop through the data and add a row for each ticket
                 data.forEach(ticket => {
                     const row = table.insertRow();
                     row.insertCell().innerHTML = ticket.id;
@@ -22,7 +68,9 @@ function fetchAllTickets() {
                     });
                     row.insertCell().appendChild(button);
                 });
+
             });
+    }, 300);
     }
 
     function fetchTicketById(id) {
@@ -53,3 +101,14 @@ function fetchAllTickets() {
     allTicketsButton.addEventListener('click', () => {
         fetchAllTickets();
     });
+function showTicketForm() {
+    document.getElementById("ticket-table").style.display = "none";
+    document.getElementById("ticket-form").style.display = "block";
+    document.getElementById("back-button").style.display = "block";
+}
+
+function hideTicketForm() {
+    document.getElementById("ticket-table").style.display = "block";
+    document.getElementById("ticket-form").style.display = "none";
+    document.getElementById("back-button").style.display = "none";
+}
--- ticketRouter.js
+++ ticketRouter.js
@@ -10,10 +10,9 @@ function getLastTicketId() {
     if (tickets.length > 0) {
         const lastTicket = tickets[tickets.length - 1];
         const lastId = parseInt(lastTicket.id, 10);
-        if (!isNaN(lastId)) {
-            return lastId;
-        }
+        return lastId+1;
     }
+
     return 0;
 }
 fs.readFile(ticketfile, 'utf8', (err, data) => {
@@ -62,15 +61,14 @@ router.post('/rest/ticket', (req, res) => {
         status,
         creator,
         created_at,
-        updated_at,
     } = req.body;
 
-    if (!type || !subject || !description || !priority || !status || !creator || !created_at || !updated_at) {
+    if (!type || !subject || !description || !priority || !status || !creator || !created_at ) {
         res.status(400).send('Incomplete ticket info');
     } else {
         const lastId = getLastTicketId();
         const newTicket = {
-            id: lastId + 1,
+            id: lastId.toString(),
             type,
             subject,
             description,
@@ -78,9 +76,8 @@ router.post('/rest/ticket', (req, res) => {
             status,
             creator,
             created_at,
-            updated_at,
         };
-
+        tickets.push(newTicket);
 
         // Write the updated tickets data to the file
         fs.writeFile(ticketfile, JSON.stringify(tickets), 'utf8', (err) => {
@@ -88,7 +85,7 @@ router.post('/rest/ticket', (req, res) => {
                 console.error(err);
                 res.status(500).send('Error writing to file');
             } else {
-                tickets.push(newTicket);
+
                 res.send(newTicket);
             }
         });
@@ -120,116 +117,3 @@ module.exports = router;
 
 
 
-/*
-const express = require('express');
-const router = express.Router();
-const bodyParser = require('body-parser');
-const tickets = require('./ticket.json');
-const fs = require('fs');
-
-// Read the tickets data from the file on startup
-const ticketfile = './ticket.json';
-let file = [];
-function getLastTicketId() {
-    if (tickets.length > 0) {
-        return tickets[tickets.length - 1].id;
-    } else {
-        return 0;
-    }
-}
-fs.readFile(ticketfile, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-    } else {
-        file = JSON.parse(data);
-    }
-});
-router.use(bodyParser.json());
-router.use(express.static('pages'));
-router.use(express.static('./'));
-//Default
-router.get('/', (req, res) =>{
-
-
-    res.send(tickets);
-});
-
-// Endpoint to get all tickets
-router.get('/rest/list', (req, res) => {
-    res.send(tickets);
-
-});
-
-// Endpoint to get a single ticket by id
-router.get('/rest/ticket/:id', (req, res) => {
-    const id = req.params.id;
-    const ticket = tickets.find((t) => t.id === id);
-
-    if (!ticket) {
-        res.status(404).send('Ticket not found');
-    } else {
-        res.send(ticket);
-    }
-});
-
-// Endpoint to create a new ticket
-router.post('/rest/ticket', (req, res) => {
-    const {
-        type,
-        subject,
-        description,
-        priority,
-        status,
-        creator,
-        created_at,
-        updated_at,
-    } = req.body;
-
-    if (!type || !subject || !description || !priority || !status || !creator || !created_at || !updated_at) {
-        res.status(400).send('Incomplete ticket info');
-    } else {
-        const lastId = getLastTicketId();
-        const newTicket = {
-            id: lastId + 1,
-            type,
-            subject,
-            description,
-            priority,
-            status,
-            creator,
-            created_at,
-            updated_at,
-        };
-     //   tickets.push(newTicket);
-     //   res.send(newTicket);
-        // Write the updated tickets data to the file
-        fs.writeFile(ticketfile, JSON.stringify(file), 'utf8', (err) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Error writing to file');
-            } else {
-                res.send(newTicket);
-            }
-        });
-    }
-});
-// Error handling middleware for bad requests
-router.use((err, req, res, next) => {
-    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-        res.status(400).send('Bad request');
-    } else {
-        next();
-    }
-});
-
-// Error handling middleware for no data to send back
-router.use((req, res, next) => {
-    if (res.statusCode === 200 && !res.headersSent) {
-        res.status(404).send('No data to send back');
-    } else {
-        next();
-    }
-});
-
-module.exports = router;
-*/
